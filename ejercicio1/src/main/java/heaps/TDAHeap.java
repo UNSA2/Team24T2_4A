51fd11f5a097655acbe51d6a63c20974aa9d11af
mramirezcc <mramirezcc@unsa.edu.pe>
2024-06-14 21:23:06 -0500

    Primer intento de metodo enqueue() y toString() implementado para hacer pruebas y seguimientos

--- ejercicio1/src/main/java/heaps/TDAHeap.java
+++ ejercicio1/src/main/java/heaps/TDAHeap.java
@@ -1,9 +1,23 @@
 package heaps;
 import interfaces.Heap;
+import java.util.ArrayList;
 
-public class TDAHeap<T> implements Heap<T> {
+public class TDAHeap<T extends Comparable<T>> implements Heap<T> {
+    ArrayList<T> data;
+
+    public TDAHeap(){
+        this.data = new ArrayList<T>();
+    }
+
     public void enqueue(T x){
+        this.data.add(x);
+        T aux;
 
+        for(int i = this.data.size(); i > 1 && this.data.get(i).compareTo(this.data.get(i/2)) > 0; j /= 2){
+            aux = this.data.get(i);
+            this.data.set(i, this.data.get(j/2));
+            this.data.set(j/2, aux);    
+        }
     }
 
     public T dequeue(){
@@ -15,6 +29,14 @@ public class TDAHeap<T> implements Heap<T> {
     }
 
     public T back(){
-        
+
+    }
+
+    public String toString(){
+        String str = "Elementos: ";
+        for(T elem : this.data){
+            str += elem.toString();
+        }
+        return str;
     }
 }
